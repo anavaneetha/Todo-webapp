3231d0b9d6f45b44622545f733b9ce819d99d4e7
anavaneetha <navaneetha.andhe@gmail.com>
2022-11-24 09:58:53 +0530

    todo webappt

---
+++ mytodo/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import MyTodo
+
+
+# Register your models here.
+@admin.register(MyTodo)
+class MyTodoAdmin(admin.ModelAdmin):
+    list_display = ['title', 'complete', 'created_at']
+    list_filter = ['complete', 'created_at']
+    readonly_fields = ['created_at']
